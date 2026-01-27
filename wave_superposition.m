57ad0b249a1d2b33cc8e4cbd5f7868ad0e5da9e8
Somay's Laptop <somay052407@gmail.com>
2026-01-27 11:49:40 -0500

    Added code to generate multiple sine waves with different amplitudes, periods, and phase shifts. These sine waves are displayed individually and as a sum

--- wave_superposition.m
+++ wave_superposition.m
@@ -1,3 +1,14 @@
-x = linspace(0,10,1000);
-y = x.^2;
-plot(x,y);
+clc, clearvars
+
+figure;
+list = zeros(1,100);
+for i = 1:10
+    x=linspace(-40,40);
+    y=sin((rand()* x) + rand()) * rand();
+    subplot(2,5,i);
+    plot(x,y);
+    list(1,:) = list(1,:) + y;
+end
+figure;
+x = linspace(-40,40);
+plot(x,list);
