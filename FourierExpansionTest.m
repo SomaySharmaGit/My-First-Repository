47da62279ef0712d74cbe0363c0ec29eddc5214a
SomaySharmaGit <somay052407@gmail.com>
2026-03-08 21:48:25 -0400

    feat: added a fourier analysis of the amplitude versus frequency for various sin waves

--- FourierExpansionTest.m
+++ FourierExpansionTest.m
@@ -6,53 +6,36 @@ y = sin(x);
 
 n = 40;
 
-a = zeros(1,n+1);
-b = zeros(1+n);
 
 reduced = zeros(n,n+1);
-
-
-
 values = linspace(-10,10,n);
+outputs = sin(2*values);
 
-outputs =  zeros(1,n);
+
+reduced(:,1)=1;
+reduced(:,n+1) = outputs;
+
 
 for i=1:n
-    outputs(i) = sin(2*values(i));
-
+    for j=2:n
+        if(mod(j,2)==0)
+            reduced(i,j) = cos((j/2) * values(i));
+        else
+            reduced(i,j) = sin((j-1)/2 * values(i));
+        end
+    end
 end
 
 
-for i=1:n
-    for j=1:n+1
-        if(j == 1)
-            reduced(i,j) = 1; %%Column
-        elseif (j==n+1)
-            reduced(i,j) = outputs(i); 
-        elseif (mod(j,2) == 0)
-            reduced(i,j) = cos((j/2) * values(i)); %% Cos col 
-        else
-            reduced(i,j) = sin((j-1)/2 * values(i)); %% Sin col 
-        end
-    end
-end    
-
 R = rref(reduced);
-disp(R);
-
 
 constants = R(:,n+1)';
-disp(constants);
 
 temp = linspace(-20,20,1000);
 y = approx(constants, n, temp);
 plot(temp,y);
 hold();
 plot(values,outputs);
-
-outputs
-
-
 
 function r = approx(constants, n, space)
     r = zeros(1,length(space)) + constants(1);
--- wave_superposition.m
+++ wave_superposition.m
@@ -1,21 +1,78 @@
 clc, clearvars
 
-figure;
-list = zeros(1,100);
+sum = zeros(1,100);
+n = 10;
 
-angularFrequency = rand(1,10);
-phaseShift = rand(1,10);
-amplitude = rand(1,10);
+angularFrequency =  rand(1,n)*10;
+phaseShift = rand(1,n)*5;
+phaseShift = zeros(1,n);
+amplitude = rand(1,n)*10;
 
-for i = 1:10
-    x=linspace(-40,40);
-    y=sin((angularFrequency(i)* x) +  phaseShift(i)) * amplitude(i);
-    subplot(2,5,i);
-    plot(x,y);
-    list(1,:) = list(1,:) + y;
+perfectSquare = 1;
+while 1 
+   
+    if (perfectSquare+1)^2 > n
+        perfectSquare = perfectSquare+1;
+        break
+    end
+     perfectSquare = perfectSquare+1;
+    
+    
 end
+disp(perfectSquare);
+
 figure;
-x = linspace(-40,40);
-plot(x,list);
+
+for i=1:n
+    x = linspace(1,100);
+    y = amplitude(i) * sin(angularFrequency(i) * x + phaseShift(i));
+    subplot(perfectSquare,perfectSquare,i);
+    plot(x,y);
+    sum(1,:) = sum(1,:) + y;
+end
+
+figure;
+x = linspace(1,100);
+plot(x,sum);
+
+
+reduced = zeros(n,n+1);
+reduced(:,1)=1;
+reduced(:,n+1) = amplitude;
+
+for i=1:n
+    for j=2:n
+        if(mod(j,2)==0)
+            reduced(i,j) = cos((j/2) * angularFrequency(i));
+        else
+            reduced(i,j) = sin((j-1)/2 * angularFrequency(i));
+        end
+    end
+end
+
+R = rref(reduced);
+
+constants = R(:,n+1)';
+
+temp = linspace(min(angularFrequency),max(angularFrequency),1000);
+y = approx(constants, n, temp);
+figure;
+plot(temp,y);
+hold("on");
+plot(angularFrequency,amplitude, '.')
+
+
+
+
+function r = approx(constants, n, space)
+    r = zeros(1,length(space)) + constants(1);
+    for i=1:n
+        if(mod(i,2) == 0)
+           r = r + (constants(i) .* cos(i/2 .* space));
+        else
+           r = r + (constants(i) .* sin((i-1)/2 .* space));
+        end
+    end
+end
 
 
